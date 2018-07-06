9f7971dbbffa66d40005502d7fad415c31727f03
partha <partha@partha-VirtualBox>
2018-07-05 23:49:47 -0500

    Solved

---
+++ CodeAbbey/src/com/codeabbey/solutions/CaesarShiftCipher.java
@@ -0,0 +1,52 @@
+package com.codeabbey.solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CaesarShiftCipher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		String firstInput = input.nextLine();
+		int numberOfInputs = Integer.parseInt(firstInput.split(" ")[0]);
+		int k = Integer.parseInt(firstInput.split(" ")[1]);
+		ArrayList<String> codes = new ArrayList<String>();
+
+		for (int i = 0; i < numberOfInputs; i++) {
+			codes.add(input.nextLine());
+		}
+
+		input.close();
+		int idx;
+		String[] breaker = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+		ArrayList<StringBuffer> result = new ArrayList<StringBuffer>();
+		StringBuffer answer = new StringBuffer();
+
+		for (String code : codes) {
+			answer = new StringBuffer();
+			for (int i = 0; i < code.length() - 1; i++) {
+				if (code.charAt(i) == ' ') {
+					answer.append(" ");
+				} else {
+					idx = Arrays.binarySearch(breaker, Character.toString((code.charAt(i)))) - k;
+					if (idx < 0) {
+						idx = 26 + idx;
+					}
+					answer.append(breaker[idx % 26]);
+				}
+			}
+			answer.append('.');
+			result.add(answer);
+		}
+
+		for (StringBuffer sb : result) {
+			System.out.print(sb.toString() + " ");
+		}
+
+	}
+
+}
---
+++ CodeAbbey/src/com/codeabbey/solutions/MortgageCalculator.java
@@ -0,0 +1,25 @@
+package com.codeabbey.solutions;
+
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class MortgageCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String problem = input.nextLine();
+		input.close();
+		
+		
+		double loan = Double.parseDouble(problem.split(" ")[0]);
+		float ratePA = Float.parseFloat(problem.split(" ")[1]);
+		int months = Integer.parseInt(problem.split(" ")[2]);
+		float ratePM = (ratePA / 1200);
+		
+		System.out.println((int) Math.ceil( (loan * ratePM * Math.pow( 1+ratePM, months)) / ( Math.pow( (1+ratePM), months)-1)) );
+	}
+
+}
