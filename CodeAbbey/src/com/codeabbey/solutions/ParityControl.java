3d42b8a4e4353f792b104291e39b42cc76ec92c6
sarathin <sarathi.n@gmail.com>
2018-07-07 13:31:22 -0500

    solved

--- CodeAbbey/src/com/codeabbey/solutions/ParityControl.java
+++ CodeAbbey/src/com/codeabbey/solutions/ParityControl.java
@@ -10,48 +10,49 @@ public class ParityControl {
 
 		String[] pStatement = input.nextLine().split(" ");
 		String fmt = new String();
-		StringBuffer tmp ;
+		StringBuffer tmp;
 		input.close();
 		String binary = new String();
+		StringBuffer answer = new StringBuffer();
 
-		for (int x = 0; x < pStatement.length-1; x++) {
+		for (int x = 0; x < pStatement.length - 1; x++) {
 			// convert string to binary value
 			binary = Integer.toBinaryString(Integer.parseInt(pStatement[x]));
-			System.out.print(pStatement[x] + "\t" + binary.length() + "\t");
 			if (binary.length() != 8) {
-				fmt = "%" + (8 - binary.length()) + "s";
-				binary = String.format(fmt, "0") + binary;
+				fmt = "%0" + (8 - binary.length()) + "d";
+				binary = String.format(fmt, 0) + binary;
 			}
-			System.out.print(binary.length() + "\t" + binary + "\t");
-			
+
 			if (isOdd(binary)) {
 				if (binary.charAt(0) == '1') {
-					 tmp = new StringBuffer(binary);
-					 binary = tmp.replace(0, 1, "0").toString();
-					 System.out.print( "\t" + binary);
-				}else {
-					System.out.print("\t        ");
+					tmp = new StringBuffer(binary);
+					binary = tmp.replace(0, 1, "0").toString();
 				}
-				System.out.println("\t" + Integer.parseInt(binary, 2) + "\t" + (char) Integer.parseInt(binary, 2));
-				
-			}else {
-				System.out.println("");
+				answer.append(Character.toString((char) Integer.parseInt(binary, 2)));
+
 			}
 		}
-
+		System.out.println(answer.toString() + ".");
 	}
-	
+
 	static boolean isOdd(String v) {
-		
-		int total=0;
-		
-		
-		for(int x=0; x<v.length(); x++) {
-			total+= Integer.parseInt( Character.toString( v.charAt(x)));
+
+		int total = 0;
+
+		try {
+			for (int x = 0; x < v.length(); x++) {
+				total += Integer.parseInt(Character.toString(v.charAt(x)));
+			}
+		} catch (Exception e) {
+			System.out.println("\n\nException occured\n\n");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			System.out.println("=========================");
+			return false;
 		}
-		
-		return (total%2 == 0);
-		
+
+		return (total % 2 == 0);
+
 	}
 
 }
