0ac0ed61ebd4306b01009531bd954de8fabbf861
vm-usr <vm-usr@localhost>
2020-04-22 20:32:45 -0500

    Add suppress annotation

--- CodeAbbey/src/com/codeabbey/solutions/QuadraticEquation.java
+++ CodeAbbey/src/com/codeabbey/solutions/QuadraticEquation.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class QuadraticEquation {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		int i;
 
